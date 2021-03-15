de6c0d39cb2dfb4482690eb560a54e6dde9db704
Everton Pinheiro <evertonpinheiroti@hotmail.com>
2021-03-15 13:19:19 -0300

    Update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import '../styles/header.scss'
 
+
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src="https://raw.githubusercontent.com/TIEverton/ignite-challenge1/main/public/logo.svg" alt="to.do" />
       </div>
     </header>
   )
